3ff535a278d88e6fcff4d94403ad9480f7085a7a
Claycuy <unggaslauncher@gmail.com>
2026-06-16 07:29:40 +0800

    chore: Update ESBuild to ESM

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,14 +3,23 @@ import esbuild from 'esbuild';
 async function build() {
   await esbuild.build({
     entryPoints: ['src/index.ts', 'src/push-engine.ts'],
-    bundle: true,        // INI PENTING: Masukin semua node_modules ke bundle
-    platform: 'node',    // Targetnya harus node
-    target: 'node22',    // Samain sama versi runner GitHub Action
-    format: 'cjs',
+    bundle: true,
+    platform: 'node',
+    target: 'node22',
+    format: 'esm', // Ubah ke esm
     outdir: 'dist',
     minify: true,
     sourcemap: false,
-    external: [],        // KOSONGIN: Biar dia "nelen" semua dep ke dalem bundle
+    external: [],
+    // Ngakalin __dirname dan __filename biar gak error di format ESM
+    banner: {
+      js: `
+        import { fileURLToPath } from 'url';
+        import { dirname } from 'path';
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+      `,
+    },
   });
 }
 
